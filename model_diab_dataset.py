9318e8a00f36f6884d4cde2f83565eda3d22ee85
Sahith Aitha <sahithaitha@Mac.lan>
2025-03-19 16:27:08 -0400

    Loaded the dataset and truncated the irrelevant columns.

--- model_diab_dataset.py
+++ model_diab_dataset.py
@@ -2,3 +2,24 @@
 from google.colab import drive
 drive.mount('/content/drive')
 
+# Importing dependencies
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+
+# path to the dataset
+file_path = '/content/drive/MyDrive/Volunteership/Diabetes_dataset/data.csv'
+try:
+  data = pd.read_csv(file_path)
+except FileNotFoundError:
+  print(f"Error: File not found at {file_path}")
+data.head()
+
+# List of irrelevant columns in the dataset
+irr_columns = ['encounter_id',
+                  'patient_nbr',
+                  'race','payer_code']
+
+# Dropping irrelevant columns
+data = data.drop(columns=irr_columns)
